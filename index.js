e112e484cbc35fe7f98b42555f313fa8c25fdced
Karl Malaga <malaga.karlbenedict@gmail.com>
2025-09-16 00:02:18 +0800

    initial commit: no random letters. basic layout

---
+++ index.js
@@ -0,0 +1,27 @@
+const l1 = document.getElementById('r1l1')
+const l2 = document.getElementById('r1l2')
+const l3 = document.getElementById('r1l3')
+const l4 = document.getElementById('r2l1')
+const l5 = document.getElementById('r2l2')
+const l6 = document.getElementById('r2l3')
+const l7 = document.getElementById('c1l1')
+const l8 = document.getElementById('c1l2')
+const l9 = document.getElementById('c1l3')
+const l10 = document.getElementById('c2l1')
+const l11 = document.getElementById('c2l2')
+const l12 = document.getElementById('c2l3')
+
+const letters = 'abcdefghijklmnopqrstuvwxyz'
+
+l1.innerHTML ='a'
+l2.innerHTML ='b'
+l3.innerHTML ='c'
+l4.innerHTML ='d'
+l5.innerHTML ='e'
+l6.innerHTML ='f'
+l7.innerHTML ='g'
+l8.innerHTML ='h'
+l9.innerHTML ='i'
+l10.innerHTML = 'j'
+l11.innerHTML = 'k'
+l12.innerHTML = 'l'
