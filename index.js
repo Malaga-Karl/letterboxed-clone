620dd5eed22c34b6a80fa75c76d1b2545ef7c19d
Karl Malaga <malaga.karlbenedict@gmail.com>
2025-09-16 01:49:38 +0800

    letter fucntionality

--- index.js
+++ index.js
@@ -10,18 +10,68 @@ const l9 = document.getElementById('c1l3')
 const l10 = document.getElementById('c2l1')
 const l11 = document.getElementById('c2l2')
 const l12 = document.getElementById('c2l3')
+const wordInput = document.getElementById('wordinput');
 
-const letters = 'abcdefghijklmnopqrstuvwxyz'
 
-l1.innerHTML ='a'
-l2.innerHTML ='b'
-l3.innerHTML ='c'
-l4.innerHTML ='d'
-l5.innerHTML ='e'
-l6.innerHTML ='f'
-l7.innerHTML ='g'
-l8.innerHTML ='h'
-l9.innerHTML ='i'
-l10.innerHTML = 'j'
-l11.innerHTML = 'k'
-l12.innerHTML = 'l'
+
+const letters = 'abcdefghijklmnopqrstuvwxyz'.split("")
+
+function pickRandom(){
+    const index = Math.floor(Math.random() * letters.length);
+    return letters.splice(index, 1)[0];
+}
+
+
+
+r1 = [pickRandom(),pickRandom(),pickRandom()]
+r2 = [pickRandom(),pickRandom(),pickRandom()]
+c1 = [pickRandom(),pickRandom(),pickRandom()]
+c2 = [pickRandom(),pickRandom(),pickRandom()]
+
+let selectedLetters = [...r1, ...r2, ...c1, ...c2]
+
+l1.innerHTML = r1[0]
+l2.innerHTML = r1[1]
+l3.innerHTML = r1[2]
+l4.innerHTML = r2[0]
+l5.innerHTML = r2[1]
+l6.innerHTML = r2[2]
+l7.innerHTML = c1[0]
+l8.innerHTML = c1[1]
+l9.innerHTML = c1[2]
+l10.innerHTML = c2[0]
+l11.innerHTML = c2[1]
+l12.innerHTML = c2[2]
+
+const allowedControlKeys = [
+    'Backspace',
+    'Delete',
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowUp',
+    'ArrowDown',
+    'Enter',
+    ' '
+]
+
+let prev = null
+
+wordInput.addEventListener('keydown', (e) => {
+    // only on selected words
+    let key = e.key.toLowerCase()
+
+    if(selectedLetters.includes(key) || allowedControlKeys.includes(e.key)){
+        if (prev){
+            if(r1.includes(key) && r1.includes(prev)){
+                    e.preventDefault()
+                }
+            }
+        console.log(`prev: ${prev}`)
+        prev = key
+    } else{
+        e.preventDefault()
+    }
+
+    // only allow outside of row/col
+    
+})
